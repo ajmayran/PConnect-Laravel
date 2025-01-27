cecee98cb504c923954830d66c6686782de45591
Alvan Jay Mayran <clickerz09@gmail.com>
2025-01-27 22:36:13 +0800

    added

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function credential()
+    {
+        return $this->hasOne(Credential::class, 'user_id');
+    }
 }
