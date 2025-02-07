4a9b7a0c677afdbca164d2020ff0bf38e3acf4ed
Matchew <119182397+Matchewmau@users.noreply.github.com>
2025-02-07 16:14:22 +0800

    added distributor page and component card

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Auth\SocialAuthController;
+use App\Http\Controllers\DistributorPageController;
 use App\Http\Controllers\Retailers\RetailerDashboardController;
 
 Route::get('/', function () {
@@ -34,12 +35,10 @@ Route::get('auth/google', [SocialAuthController::class, 'googleRedirect'])->name
 Route::get('auth/google/callback', [SocialAuthController::class, 'googleCallback']);
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', [RetailerDashboardController::class, 'index'])->name('dashboard');
 
     // Add distributor route
-    Route::get('/distributor/{id}', [DistributorPageController::class, 'show'])
+    Route::get('/distributor', [DistributorPageController::class, 'show'])
         ->name('distributor.show');
 });
 
