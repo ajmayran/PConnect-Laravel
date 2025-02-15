429b0a415f83c3f57827d50641cdeb181bff2252
Chongk3y <jazzerong214@gmail.com>
2025-02-16 02:09:44 +0800

    retailer UI fixed and added sidebar, except for feild color

---
+++ resources/views/components/retailer-sidebar.blade.php
@@ -0,0 +1,14 @@
+<div class="container flex gap-2  mx-auto">
+    <div class="w-1/4 h-screen pt-5 shadow-md bg-white sm:rounded-lg ">
+      <h2 class="p-4 mb-4 text-xl font-bold text-center">Account</h2>
+        <ul class="text-center tab-list space-y">
+            <li class="p-2 cursor-pointer tab-item hover:bg-gray-200">Profile</li>
+            <li class="p-2 cursor-pointer tab-item hover:bg-gray-200 ">Address</li>
+            <li class="p-2 cursor-pointer tab-item hover:bg-gray-200 ">Change Password</li>
+            <li class="p-2 cursor-pointer tab-item hover:bg-gray-200 ">My Purchase</li>
+            <li class="p-2 cursor-pointer tab-item hover:bg-gray-200 " >Notifications</li>
+            <li class="p-2 cursor-pointer tab-item hover:bg-gray-200 ">Messages</li>
+            <li class="p-2 cursor-pointer tab-item hover:bg-gray-200">My Voucher</li>
+            <li class="p-2 mt-2 font-bold text-red-500 cursor-pointer tab-item hover:bg-gray-200">Log Out</li>
+        </ul>
+    </div>
--- resources/views/components/retailer-topnav.blade.php
+++ resources/views/components/retailer-topnav.blade.php
@@ -10,18 +10,6 @@
                         <img class="w-auto h-10" src="{{ asset('img/Pconnect Logo.png') }}" alt="PConnect">
                     </div>
                 </div>
-
-                <!-- Search -->
-                <div class="flex items-center ml-6">
-                    <input type="text" placeholder="Search for items..."
-                        class="px-4 py-2 border-gray-300 w-96 border-y focus:ring-green-500 focus:border-green-500">
-                    <button class="px-6 py-2.5 text-white bg-green-500 rounded-r hover:bg-green-600">
-                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                        </svg>
-                    </button>
-                </div>
             </div>
 
             <!-- Right Side -->
