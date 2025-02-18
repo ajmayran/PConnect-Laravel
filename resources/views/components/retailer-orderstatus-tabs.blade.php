d700d6497f5224c0b87a17c728716a3fc22b2bc7
Chongk3y <jazzerong214@gmail.com>
2025-02-19 03:05:35 +0800

    added retailer-orderstatus-tabs for components

---
+++ resources/views/components/retailer-orderstatus-tabs.blade.php
@@ -0,0 +1,8 @@
+<ul class="flex border-b border-white tab-list">
+    <a href="" class="flex-1 py-2 font-semibold text-center text-white bg-green-500 tab-item">All</a>
+    <a href="" class="flex-1 py-2 text-center cursor-pointer tab-item bg-white hover:bg-gray-400">To Pay</a>
+    <a href="" class="flex-1 py-2 text-center cursor-pointer tab-item bg-white hover:bg-gray-400">To Receive</a>
+    <a href="" class="flex-1 py-2 text-center cursor-pointer tab-item bg-white hover:bg-gray-400">Completed</a>
+    <a href="" class="flex-1 py-2 text-center cursor-pointer tab-item bg-white hover:bg-gray-400">Cancelled</a>
+    <a href="" class="flex-1 py-2 text-center cursor-pointer tab-item bg-white hover:bg-gray-400">Return/Refund</a>
+</ul>
